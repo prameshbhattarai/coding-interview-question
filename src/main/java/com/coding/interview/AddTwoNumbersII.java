3a9eaffea454cfdbd598729dabab879516c77a8b
pramesh.bhattarai <pra4mesh@gmail.com>
2022-01-30 12:40:56 +0545

    * Add Two Numbers in ListNode.

---
+++ src/main/java/com/coding/interview/AddTwoNumbersII.java
@@ -0,0 +1,93 @@
+package com.coding.interview;
+
+import java.util.Objects;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Input: l1 = [7,2,4,3], l2 = [5,6,4]
+ * Output: [7,8,0,7]
+ */
+public class AddTwoNumbersII {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this(val);
+            this.next = next;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ListNode listNode = (ListNode) o;
+            return val == listNode.val && Objects.equals(next, listNode.next);
+        }
+    }
+
+    public ListNode addTwoNumber(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        ListNode result = add(l1, l2);
+        return reverse(result);
+    }
+
+    private ListNode add(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode head = result;
+        int sum = 0;
+        while (l1 != null || l2 != null) {
+            sum = sum / 10;
+
+            if (l1 != null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+
+            result.next = new ListNode(sum % 10);
+            result = result.next;
+        }
+
+        if (sum / 10 == 1) {
+            result.next = new ListNode(1);
+        }
+        return head.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+
+            head.next = prev; // reverse the next to prev
+
+            prev = head; // current node is prev node
+            head = next; // move the head to next node
+        }
+        return prev;
+    }
+
+}
---
+++ src/test/java/com/coding/interview/AddTwoNumbersIITest.java
@@ -0,0 +1,55 @@
+package com.coding.interview;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AddTwoNumbersIITest {
+
+    public static final AddTwoNumbersII  addTwoNumbersII = new AddTwoNumbersII();
+
+    public static final AddTwoNumbersII.ListNode l1 =
+    new AddTwoNumbersII.ListNode(7,
+            new AddTwoNumbersII.ListNode(2,
+                    new AddTwoNumbersII.ListNode(4,
+                            new AddTwoNumbersII.ListNode(3)
+                    )
+            )
+    );
+
+    public static final AddTwoNumbersII.ListNode l2 =
+    new AddTwoNumbersII.ListNode(5,
+            new AddTwoNumbersII.ListNode(6,
+                    new AddTwoNumbersII.ListNode(4)
+            )
+    );
+
+    public static final AddTwoNumbersII.ListNode expectedResult =
+    new AddTwoNumbersII.ListNode(7,
+            new AddTwoNumbersII.ListNode(8,
+                    new AddTwoNumbersII.ListNode(0,
+                            new AddTwoNumbersII.ListNode(7)
+                    )
+            )
+    );
+
+    public static final AddTwoNumbersII.ListNode l3 =
+            new AddTwoNumbersII.ListNode(5);
+
+    public static final AddTwoNumbersII.ListNode l4 =
+            new AddTwoNumbersII.ListNode(5);
+
+    public static final AddTwoNumbersII.ListNode expectedResult2 =
+            new AddTwoNumbersII.ListNode(1,
+                    new AddTwoNumbersII.ListNode(0)
+            );
+
+    @Test
+    public void addNodeTest1() {
+        AddTwoNumbersII.ListNode actualResult = addTwoNumbersII.addTwoNumber(l1, l2);
+        Assertions.assertEquals(expectedResult, actualResult);
+
+        actualResult = addTwoNumbersII.addTwoNumber(l3, l4);
+        Assertions.assertEquals(expectedResult2, actualResult);
+    }
+
+}
